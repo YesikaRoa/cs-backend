db180599a62dd5fd49fb2d1ec25fb7143d0c4f8e
Yesika Roa <yroa1402@gmail.com>
2025-05-25 17:09:50 -0400

    Cambie el status por default del model Post a 'pending_approval', agregue el nuevo enum 'pending_approval', y modifique el servicio de post para crear un post, porque si no enviaba una community_id, por defecto me enviaba 1, entonces deje por defecto la del user que esta creando el post.

--- src/schemas/posts.schema.js
+++ src/schemas/posts.schema.js
@@ -4,6 +4,8 @@ export const createPostSchema = z.object({
   title: z.string().min(1, { message: 'El título es requerido' }),
   content: z.string().min(1, { message: 'El contenido es requerido' }),
   category_id: z.number({ message: 'La categoría es requerida' }),
+  community_id: z.number().optional(),
+  status: z.enum(['draft', 'published', 'pending_approval']).optional(), // Hacer que sea opcional
 })
 
 export const updatePostSchema = createPostSchema.partial()
