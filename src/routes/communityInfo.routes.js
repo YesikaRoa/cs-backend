a98e5949fe4b3f73ed64b7f0704b85b0b63962ee
Barragan <jcbarragan1994@gmail.com>
2025-06-09 01:11:49 -0400

    feat: add unique constraint to CommunityInformation title and update related services and routes

--- src/routes/communityInfo.routes.js
+++ src/routes/communityInfo.routes.js
@@ -1,17 +1,12 @@
 import express from 'express'
 import {
   getCommunityInfo,
-  getCommunityInfoById,
-  createCommunityInfo,
+  getCommunityInfoByKey,
   updateCommunityInfo,
-  deleteCommunityInfo,
 } from '../controllers/communityInfo.controller.js'
 
 import { validate } from '../middlewares/validateInput.js'
-import {
-  CommunityInfoSchema,
-  CommunityInfoUpdateSchema,
-} from '../schemas/communityInfo.schema.js'
+import { CommunityInfoUpdateSchema } from '../schemas/communityInfo.schema.js'
 import { verifyToken } from '../middlewares/auth.js'
 
 const router = express.Router()
@@ -19,26 +14,15 @@ const router = express.Router()
 // Obtener toda la información de la comunidad
 router.get('/', getCommunityInfo)
 
-// Obtener información por ID
-router.get('/:id', getCommunityInfoById)
-
-// Crear información
-router.post(
-  '/',
-  verifyToken,
-  validate(CommunityInfoSchema),
-  createCommunityInfo
-)
+// Obtener información por Key
+router.get('/:key', getCommunityInfoByKey)
 
 // Actualizar información
 router.put(
-  '/:id',
+  '/:key',
   verifyToken,
   validate(CommunityInfoUpdateSchema),
   updateCommunityInfo
 )
-
-// Eliminar información
-router.delete('/:id', verifyToken, deleteCommunityInfo)
 
 export default router
