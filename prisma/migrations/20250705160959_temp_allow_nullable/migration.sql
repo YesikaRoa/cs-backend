50080afa17af189d67a9039f4c145b18b930a128
Barragan <jcbarragan1994@gmail.com>
2025-07-05 12:11:04 -0400

    Refactor CommunityInformation relationship: make community_id nullable and update foreign key constraint
    - Changed community_id in CommunityInformation from NOT NULL to nullable.
    - Updated foreign key constraint to set community_id to NULL on delete instead of restricting deletion.

--- prisma/migrations/20250705160959_temp_allow_nullable/migration.sql
+++ prisma/migrations/20250705160959_temp_allow_nullable/migration.sql
@@ -1,8 +1,5 @@
 -- DropForeignKey
 ALTER TABLE "CommunityInformation" DROP CONSTRAINT "CommunityInformation_community_id_fkey";
 
--- AlterTable
-ALTER TABLE "CommunityInformation" ALTER COLUMN "community_id" DROP NOT NULL;
-
 -- AddForeignKey
 ALTER TABLE "CommunityInformation" ADD CONSTRAINT "CommunityInformation_community_id_fkey" FOREIGN KEY ("community_id") REFERENCES "Community"("id") ON DELETE SET NULL ON UPDATE CASCADE;
